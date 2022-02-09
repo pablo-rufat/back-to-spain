bc4254a5bebe515bfddf3c1df2396aad29711e97
Pablo <pablo.rufat@betrybe.com>
2022-02-09 11:44:55 -0300

    pushing everything

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+const parseInterval = (interval) => {
+  switch(interval) {
+    case 'days':
+      return 'dias';
+    case 'day':
+      return 'dia';
+    case 'months':
+      return 'meses';
+    case 'month':
+      return 'mes';
+    case 'hours':
+      return 'horas';
+    case 'hour':
+      return 'hora';
+    case 'minutes':
+      return 'minutos';
+    case 'minuto':
+      return 'minuto';
+    case 'seconds':
+      return 'segundos';
+    case 'second':
+      return 'segundo';
+    default:
+      return 'tomate';
+  }
+}; 
+
+const calculateTimeLeft = () => {
+  const finalDate = new Date(2022, 2, 21, 5, 10, 0, 0);
+  const difference = ( finalDate - new Date());
+
+  let timeLeft = {};
+
+  if (difference > 0) {
+    timeLeft = {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / 1000 / 60) % 60),
+      seconds: Math.floor((difference / 1000) % 60)
+    };
+  }
+
+  return timeLeft;
+};
 
 function App() {
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  });
+
+  const timerComponents = [];
+
+  Object.keys(timeLeft).forEach((interval) => {
+    if (!timeLeft[interval]) {
+      return;
+    }
+  
+    timerComponents.push(
+      <span>
+        {timeLeft[interval]} {parseInterval(interval)}{" "}
+      </span>
+    );
+  });
+
+  if (!timerComponents[3]) timerComponents[3] = <span> {0} segundos {" "} </span>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="background" class="d-flex flex-column align-items-center justify-content-center">
+      <h1 id="title" class="d-flex">CUENTA ATRÁS PARA LA VUELTA A ESPAÑA!</h1>
+      <div id="timer" class="flex-wrap d-flex justify-content-center">
+        <div id="days" class="align-items-center flex-column d-flex justify-content-center">
+          { timerComponents[0] }
+        </div>
+        <div id="hours" class="align-items-center flex-column d-flex justify-content-center">
+          { timerComponents[1] }
+        </div>
+        <div id="minutes" class="align-items-center flex-column d-flex justify-content-center">
+          { timerComponents[2] }
+        </div>
+        <div id="seconds" class="align-items-center flex-column d-flex justify-content-center">
+          { timerComponents[3] }
+        </div>
+      </div>
     </div>
   );
 }
